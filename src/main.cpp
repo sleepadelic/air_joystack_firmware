035890c836a24ade21e81851ec57eda0d6f53702
sleepadelic <sleepadelic@gmail.com>
2023-10-22 03:49:45 +0600

    joystick

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 #include <EncButton.h>
 #include <GyverHC595.h>
 
-
-// Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
-//                    JOYSTICK_TYPE_MULTI_AXIS, 32, 0,
-//                    true, true, false, false, false, false,
-//                    true, true, false, false, false);
+// Init
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
+                   JOYSTICK_TYPE_MULTI_AXIS, 15, 0,
+                   true, true, true, true, true, true,
+                   false, false, false, false, false);
 Button button_1 = Button(B1_PIN);
 Button button_2 = Button(B2_PIN);
 Button button_3 = Button(B3_PIN);
@@ -19,9 +19,17 @@ Encoder enc_3 = Encoder(E31_PIN, E32_PIN, INPUT_PULLUP);
 
 GyverHC595<1, HC_PINS> led_reg(latchPin, dataPin, clockPin);
 
-
 void setup()
 {
+    // Joystick settings
+    Joystick.begin();
+    Joystick.setXAxisRange(0, 600);
+    Joystick.setRxAxisRange(0, 600);
+    Joystick.setYAxisRange(0, 600);
+    Joystick.setRyAxisRange(0, 600);
+    Joystick.setZAxisRange(0, 600);
+    Joystick.setRzAxisRange(0, 600);
+    // Set potentiometers modes
     pinMode(R1_PIN, INPUT);
     pinMode(R2_PIN, INPUT);
     pinMode(R3_PIN, INPUT);
@@ -29,18 +37,22 @@ void setup()
     pinMode(R5_PIN, INPUT);
     pinMode(R6_PIN, INPUT);
 
+    // enc 2 and 3 have another signal types
     enc_2.setEncType(EB_STEP2);
     enc_3.setEncType(EB_STEP2);
+
+    // led test (init OK)
     led_reg.setAll();
-    led_reg.update(); 
+    led_reg.update();
     delay(800);
     led_reg.clearAll();
-    led_reg.update(); 
+    led_reg.update();
 }
 int value = 0;
 byte byteToSend = 0;
 void loop()
 {
+    // Buttons and enc read
     button_1.tick();
     button_2.tick();
     button_3.tick();
@@ -51,36 +63,36 @@ void loop()
 
     if (button_1.press())
     {
-        led_reg.write(2,1);
         Serial.println("b1_press");
+        Joystick.setButton(0, 1);
     }
     if (button_1.release())
     {
-        led_reg.write(2,0);
+        Joystick.setButton(0, 0);
         Serial.println("b1_release");
     }
     if (button_2.press())
-    {   
-        // led_reg.set(1);
-        led_reg.write(3,1);
+    {
+        Joystick.setButton(1, 1);
         Serial.println("b2_press");
     }
     if (button_2.release())
     {
-        led_reg.write(3,0);
+        Joystick.setButton(1, 0);
         Serial.println("b2_release");
     }
     if (button_3.press())
     {
-        // led_reg.set(2);
+        Joystick.setButton(2, 1);
         Serial.println("b3_press");
     }
     if (button_3.release())
     {
+        Joystick.setButton(2, 0);
         Serial.println("b3_release");
     }
     if (enc_0.press())
-    {   
+    {
         led_reg.set(0);
         Serial.println("e0_press");
     }
@@ -97,10 +109,13 @@ void loop()
 
     if (enc_1.press())
     {
+        led_reg.set(1);
+
         Serial.println("e1_press");
     }
     if (enc_1.release())
     {
+        led_reg.clear(1);
         Serial.println("e1_release");
     }
     if (enc_1.turn())
@@ -112,36 +127,96 @@ void loop()
     // Enc 2,3
     if (enc_2.turn())
     {
-        Serial.println("e2 turn");
-        Serial.println(enc_2.dir());
+        // Serial.println("e2 turn");
+        // Serial.println(enc_2.dir());
+        if (enc_2.dir() == 1)
+        {
+            Joystick.pressButton(3);
+            Joystick.sendState();
+            Joystick.releaseButton(3);
+            Joystick.sendState();
+        }
+        else
+        {
+            Joystick.pressButton(4);
+            Joystick.sendState();
+            Joystick.releaseButton(4);
+            Joystick.sendState();
+        }
     }
     if (enc_3.turn())
     {
-        Serial.println("e3 turn");
-        Serial.println(enc_3.dir());
+        // Serial.println("e3 turn");
+        // Serial.println(enc_3.dir());
+        if (enc_3.dir() == 1)
+        {
+            Joystick.pressButton(5);
+            Joystick.sendState();
+            Joystick.releaseButton(5);
+            Joystick.sendState();
+        }
+        else
+        {
+            Joystick.pressButton(6);
+            Joystick.sendState();
+            Joystick.releaseButton(6);
+            Joystick.sendState();
+        }
     }
-    // if (enc_3.right())
-    // {
-    //     Serial.println("e3_right");
-    // }
-
-    //     delay(100);
-    //     for (byte bitPos = 0; bitPos < 8; bitPos++) {
-    //       byteToSend = 0; // обнуляем байт, чтобы выключить светодиод
-    //       bitWrite(byteToSend, bitPos, HIGH);
-    //       digitalWrite(latchPin, LOW);                                        // ставим LOW на "защёлку"
-    //       shiftOut(dataPin, clockPin, LSBFIRST, byteToSend);
-    //       digitalWrite(latchPin, HIGH);                                       // ставим HIGH на "защёлку"
-    //       delay(100);
-    //    }
-    //    for (byte bitPos = 0; bitPos < 8; bitPos++) {
-    //       byteToSend = 0; // обнуляем байт, чтобы выключить светодиод
-    //       bitWrite(byteToSend, bitPos, HIGH);
-    //       digitalWrite(latchPin, LOW);                                        // ставим LOW на "защёлку"
-    //       shiftOut(dataPin, clockPin, MSBFIRST, byteToSend);
-    //       digitalWrite(latchPin, HIGH);                                       // ставим HIGH на "защёлку"
-    //       delay(100);
-    //    }
-
-    led_reg.update(); 
+    if (analogRead(R1_PIN) > 200)
+    {
+        led_reg.set(7);
+    }
+    else
+    {
+        led_reg.clear(7);
+    }
+    if (analogRead(R2_PIN) > 200)
+    {
+        led_reg.set(6);
+    }
+    else
+    {
+        led_reg.clear(6);
+    }
+    if (analogRead(R3_PIN) > 200)
+    {
+        led_reg.set(5);
+    }
+    else
+    {
+        led_reg.clear(5);
+    }
+    if (analogRead(R4_PIN) > 200)
+    {
+        led_reg.set(4);
+    }
+    else
+    {
+        led_reg.clear(4);
+    }
+    if (analogRead(R5_PIN) > 200)
+    {
+        led_reg.set(3);
+    }
+    else
+    {
+        led_reg.clear(3);
+    }
+    if (analogRead(R6_PIN) > 200)
+    {
+        Serial.println(analogRead(R6_PIN) >> 3);
+        led_reg.set(2);
+    }
+    else
+    {
+        led_reg.clear(2);
+    }
+    Joystick.setXAxis(analogRead(R6_PIN) >> 3);
+    Joystick.setRxAxis(analogRead(R5_PIN) >> 3);
+    Joystick.setYAxis(analogRead(R4_PIN) >> 3);
+    Joystick.setRyAxis(analogRead(R3_PIN) >> 3);
+    Joystick.setZAxis(analogRead(R2_PIN) >> 3);
+    Joystick.setRzAxis(analogRead(R1_PIN) >> 3);
+    led_reg.update();
 }
